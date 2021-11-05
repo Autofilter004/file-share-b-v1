72ff2c19884561545252dcda0a0d1176a4f20fa0
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-11-05 17:05:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -51,7 +51,7 @@ async def start_command(client: Client, message: Message):
         for msg in messages:
 
             if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
+                caption = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = "https://t.me/mhd_thanzeer")]])
             else:
                 caption = "" if not msg.caption else msg.caption.html
 
