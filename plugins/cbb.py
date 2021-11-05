35c4cfe8ae528c178cb1b3e2f98433781f57f0f3
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-11-05 17:42:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,8 +9,11 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
-        await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/solve_666/4'>Click here</a>\n○ Group : @wolfpackmedia\n○ Support Group : @wolfpackmedia </b>",
+        await query.message.reply_photo(
+
+            "https://telegra.ph/file/9547063d1d78f20e6d073.jpg",
+
+            caption = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/solve_666/4'>Click here</a>\n○ Group : @wolfpackmedia\n○ Support Group : @wolfpackmedia </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
