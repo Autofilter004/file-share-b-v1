715c0701bd2b2b0d919167fde0a789b9acba6154
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-11-05 17:34:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -77,18 +77,13 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
+        await message.reply_photo(
+            "https://telegra.ph/file/f762cedd4f9d6a4e61612.jpg",
+             caption=START_MSG.format(message.from_user.first_name),
+             reply_markup=reply_markup,
+             quote=True
+         )
+        
         return
 
 @Bot.on_message(filters.command('start') & filters.private)
